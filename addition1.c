9385fe74090ee9c0ffb5bb537c7828f089c1e00d
Ishika-Jadhav <ishikajadhav2002@gmail.com>
2024-06-26 20:31:34 -0700

    first program

---
+++ addition1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+ {
+    int No1 = 0;
+    int No2 =0;
+    int No3 = 0;
+    
+    printf("Enter first number : \n");
+    scanf("%d",&No1);
+    
+    printf("Enter second number: \n");
+    scanf("%d",&No2);
+    
+    No3 = No1 + No2;
+    
+    printf("Addition of two number is: %d\n",No3);
+    
+ 
+    return 0;
+ }
