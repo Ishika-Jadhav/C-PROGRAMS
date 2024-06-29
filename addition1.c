2bca9f6c5706587444de2db6eb07cd1af33cc644
Ishika-Jadhav <ishikajadhav2002@gmail.com>
2024-06-28 19:37:03 -0700

    addition1.c

--- addition1.c
+++ addition1.c
@@ -4,7 +4,7 @@ int main()
  {
     int No1 = 0;
     int No2 =0;
-    int No3 = 0;
+    int result = 0;
     
     printf("Enter first number : \n");
     scanf("%d",&No1);
@@ -12,9 +12,9 @@ int main()
     printf("Enter second number: \n");
     scanf("%d",&No2);
     
-    No3 = No1 + No2;
+    result = No1 + No2;
     
-    printf("Addition of two number is: %d\n",No3);
+    printf("Addition of two number is: %d\n",result);
     
  
     return 0;
